7bac37a718d67886e47bcb70f4ff5021ac888dc0
Elena Valchuk <elena.valchuk@gmail.com>
2022-08-31 20:07:52 +0300

    increment1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+    "net/http"
+)
+
+// MetricsRegistered — обработчик запроса.
+func MetricsRegistered(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("<h1>Metrics received</h1>"))
+}
+
+func main() {
+    // маршрутизация запросов обработчику
+    http.HandleFunc("/", MetricsRegistered)
+    // запуск сервера с адресом localhost, порт 8080
+    server := &http.Server{
+        Addr: "127.0.0.1:8080",
+    }
+    server.ListenAndServe()
+} 
