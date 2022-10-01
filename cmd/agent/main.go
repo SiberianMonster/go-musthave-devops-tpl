dbbaa509427688fdf73a76c5ed722394018a372f
Elena Valchuk <elena.valchuk@gmail.com>
2022-10-01 22:52:22 +0300

    removed content length

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -172,9 +172,7 @@ func ReportUpdate(p int, r int) error {
 				}
 				
 				
-				request.Header.Set("Content-Type", "application/json")
-				request.Header.Set("Content-Length", strconv.Itoa(len(body)))
-				
+				request.Header.Set("Content-Type", "application/json")				
 				response, err := client.Do(request)
 				if err != nil {
 					log.Printf("Error when response received")
