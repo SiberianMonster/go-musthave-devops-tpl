a2718ac5e4741f787c2a9f608d7e3ce8eb4e8f74
Elena Valchuk <elena.valchuk@gmail.com>
2022-10-10 13:24:24 +0300

    fixed typo

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -176,7 +176,7 @@ func StaticFileUpload(storeFile string, restore bool) {
 	}
 }
 
-func DBSave(storeDB *sql.DB, ctx context.Context) {
+func DBSave(storeDB *sql.DB) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		// не забываем освободить ресурс
