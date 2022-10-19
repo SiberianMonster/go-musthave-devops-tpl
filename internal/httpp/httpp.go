f82761454ee961b0e77f58ef61309e416796d9c5
Elena Valchuk <elena.valchuk@gmail.com>
2022-10-19 14:57:12 +0300

    removed commented function

--- internal/httpp/httpp.go
+++ internal/httpp/httpp.go
@@ -18,11 +18,6 @@ func (w GzipWriter) Write(b []byte) (int, error) {
 	return w.Writer.Write(b)
 }
 
-//func getBinaryBySHA256(s string) []byte {
-//    r := sha256.Sum256([]byte(s))
-//    return r[:]
-//}
-
 func Hash(value, key string) (string, error) {
 	mac := hmac.New(sha256.New, []byte(key))
 	_, err := mac.Write([]byte(value))
