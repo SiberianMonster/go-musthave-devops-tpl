dcece5c10f213e78aa53d3f5c1ea39886c0e1eb6
Valchuk Elena <valchukelena@Valchuks-MacBook-Pro.local>
2023-02-14 18:01:37 +0300

    updated proto files

--- cmd/server/proto/server_pr_grpc.pb.go
+++ cmd/server/proto/server_pr_grpc.pb.go
@@ -18,122 +18,122 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// ServerServiceClient is the client API for ServerService service.
+// GrpcClient is the client API for Grpc service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type ServerServiceClient interface {
+type GrpcClient interface {
 	Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*UpdateResponse, error)
 	Value(ctx context.Context, in *ValueRequest, opts ...grpc.CallOption) (*ValueResponse, error)
 }
 
-type serverServiceClient struct {
+type grpcClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewServerServiceClient(cc grpc.ClientConnInterface) ServerServiceClient {
-	return &serverServiceClient{cc}
+func NewGrpcClient(cc grpc.ClientConnInterface) GrpcClient {
+	return &grpcClient{cc}
 }
 
-func (c *serverServiceClient) Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*UpdateResponse, error) {
+func (c *grpcClient) Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*UpdateResponse, error) {
 	out := new(UpdateResponse)
-	err := c.cc.Invoke(ctx, "/main.ServerService/Update", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/main.Grpc/Update", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *serverServiceClient) Value(ctx context.Context, in *ValueRequest, opts ...grpc.CallOption) (*ValueResponse, error) {
+func (c *grpcClient) Value(ctx context.Context, in *ValueRequest, opts ...grpc.CallOption) (*ValueResponse, error) {
 	out := new(ValueResponse)
-	err := c.cc.Invoke(ctx, "/main.ServerService/Value", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/main.Grpc/Value", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// ServerServiceServer is the server API for ServerService service.
-// All implementations must embed UnimplementedServerServiceServer
+// GrpcServer is the server API for Grpc service.
+// All implementations must embed UnimplementedGrpcServer
 // for forward compatibility
-type ServerServiceServer interface {
+type GrpcServer interface {
 	Update(context.Context, *UpdateRequest) (*UpdateResponse, error)
 	Value(context.Context, *ValueRequest) (*ValueResponse, error)
-	mustEmbedUnimplementedServerServiceServer()
+	mustEmbedUnimplementedGrpcServer()
 }
 
-// UnimplementedServerServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedServerServiceServer struct {
+// UnimplementedGrpcServer must be embedded to have forward compatible implementations.
+type UnimplementedGrpcServer struct {
 }
 
-func (UnimplementedServerServiceServer) Update(context.Context, *UpdateRequest) (*UpdateResponse, error) {
+func (UnimplementedGrpcServer) Update(context.Context, *UpdateRequest) (*UpdateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
 }
-func (UnimplementedServerServiceServer) Value(context.Context, *ValueRequest) (*ValueResponse, error) {
+func (UnimplementedGrpcServer) Value(context.Context, *ValueRequest) (*ValueResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Value not implemented")
 }
-func (UnimplementedServerServiceServer) mustEmbedUnimplementedServerServiceServer() {}
+func (UnimplementedGrpcServer) mustEmbedUnimplementedGrpcServer() {}
 
-// UnsafeServerServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to ServerServiceServer will
+// UnsafeGrpcServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to GrpcServer will
 // result in compilation errors.
-type UnsafeServerServiceServer interface {
-	mustEmbedUnimplementedServerServiceServer()
+type UnsafeGrpcServer interface {
+	mustEmbedUnimplementedGrpcServer()
 }
 
-func RegisterServerServiceServer(s grpc.ServiceRegistrar, srv ServerServiceServer) {
-	s.RegisterService(&ServerService_ServiceDesc, srv)
+func RegisterGrpcServer(s grpc.ServiceRegistrar, srv GrpcServer) {
+	s.RegisterService(&Grpc_ServiceDesc, srv)
 }
 
-func _ServerService_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Grpc_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UpdateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ServerServiceServer).Update(ctx, in)
+		return srv.(GrpcServer).Update(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/main.ServerService/Update",
+		FullMethod: "/main.Grpc/Update",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServerServiceServer).Update(ctx, req.(*UpdateRequest))
+		return srv.(GrpcServer).Update(ctx, req.(*UpdateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ServerService_Value_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Grpc_Value_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ValueRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ServerServiceServer).Value(ctx, in)
+		return srv.(GrpcServer).Value(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/main.ServerService/Value",
+		FullMethod: "/main.Grpc/Value",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServerServiceServer).Value(ctx, req.(*ValueRequest))
+		return srv.(GrpcServer).Value(ctx, req.(*ValueRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// ServerService_ServiceDesc is the grpc.ServiceDesc for ServerService service.
+// Grpc_ServiceDesc is the grpc.ServiceDesc for Grpc service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var ServerService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "main.ServerService",
-	HandlerType: (*ServerServiceServer)(nil),
+var Grpc_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "main.Grpc",
+	HandlerType: (*GrpcServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Update",
-			Handler:    _ServerService_Update_Handler,
+			Handler:    _Grpc_Update_Handler,
 		},
 		{
 			MethodName: "Value",
-			Handler:    _ServerService_Value_Handler,
+			Handler:    _Grpc_Value_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
